51d8528f239ba951fb9aabb1f903526a02a162ae
Kaili <35883923+MaKaili@users.noreply.github.com>
2025-06-04 11:29:28 +0800

    minor (#15)

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def index():
         content_type=content_type,
         document_meta=document_meta,
     )
-    print(await index.query_all("tell me about bitcoin"))
+    print(await index.query_all("tell me about US healthcare system"))
 
 
 if __name__ == "__main__":
